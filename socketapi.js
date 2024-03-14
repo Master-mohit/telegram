3cd236a245c025abfb138f57efd82dffb42ebb44
Master-mohit <mohitmansigupta9098@gmail.com>
2024-03-15 01:43:04 +0530

    third commit

--- socketapi.js
+++ socketapi.js
@@ -1,22 +1,22 @@
-const io = require( "socket.io" )();
+const io = require("socket.io")();
 const socketapi = {
     io: io
 };
 
 // Add your socket.io logic here!
-io.on( "connection", function( socket ) {
-    console.log( "A user connected" );
+io.on("connection", function(socket) {
+    console.log("A user connected");
 
-    socket.on("sony", function(chacha){
-      console.log("chacha")
+    socket.on("sony", function(chacha) {
+        console.log("chacha")
+        socket.broadcast.emit("max", chacha)
+    });
 
-      socket.broadcast.emit("max", chacha)
-
-      socket.on('newPhoto', function(data){
-            console.log("data")
-      });
-    })
+    // Handle newPhoto event
+    socket.on('newPhoto', function(data) {
+        console.log("New photo received:", data.photo);
+        io.emit('postPhoto', data.photo); 
+    });
 });
-// end of socket.io logic
 
 module.exports = socketapi;
